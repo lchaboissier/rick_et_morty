ecada2afe7afb51ab8a0820e8c386394f5ca54f2
Luca Chaboissier <lucachaboissier@outlook.com>
2022-09-29 14:06:41 +0200

    position page updated

--- rick_et_morty/rick_et_morty/ViewController.swift
+++ rick_et_morty/rick_et_morty/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button: UIButton!
+    
+    @IBOutlet weak var image: UIImageView!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+            super.viewDidLoad()
+            // Do any additional setup after loading the view.
+            button.setTitle("Press me", for: .normal)
+            image.image = UIImage(named: "0.JPEG")
+        }
+
+    @IBAction func actionOnClick(_ sender: Any) {
+        print("n\'importe quoi dedans ")
+        let valeur : Int = Int.random(in: 0...100)%2
+        if valeur % 2 == 0 {
+            image.image = UIImage(named: "0.JPEG")
+        }else{
+            image.image = UIImage(named: "1.JPEG")
+        }
     }
-
-
 }
 
